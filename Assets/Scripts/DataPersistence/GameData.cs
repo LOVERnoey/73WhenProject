649cb96c0951346fbbbefd34507a9e1d6fa784f3
ratchakit <basszamama20@gmail.com>
2025-07-25 19:24:33 +0700

    Save&Load implement

---
+++ Assets/Scripts/DataPersistence/GameData.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameData
+{
+    public int countTest;
+    
+    public Vector3  playerPosition;
+    
+    public SerializableDictionary<string, bool>  itemsCollected;
+
+    public GameData()
+    {
+        this.countTest = 0;
+        playerPosition = new Vector3(0, 0.5f, 0);
+        itemsCollected = new SerializableDictionary<string, bool>();    
+    }
+}
